3f4dce980d5a3621f7fd6d2d815233658c4237c9
remedy <91361186+rremedyy@users.noreply.github.com>
2024-06-20 11:54:32 -0600

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -15,26 +15,26 @@ General:AddToggle('ToggleFlag1', {
 	Text = 'Toggle 1';
 	Default = false;
 	Callback = function(Value)
-      		print(Value)
+      		print(Value);
 	end;
 }):AddColorPicker('ColorPickerFlag1', {
 	Default = Color3.fromRGB(255,255,255);
 	Callback = function(Value)
-      		print(Value)
+      		print(Value);
 	end;
 });
 General:AddToggle('ToggleFlag2', {
 	Text = 'Toggle 2';
 	Default = true;
 	Callback = function(Value)
-      		print(Value)
+      		print(Value);
 	end;
 }):AddKeypicker('KeyPickerFlag1', {
 	Default = 'X';
 	NoUI = false; -- true will not show it on the left side
 	Mode = 'Toggle'; -- Held, Toggle
 	Callback = function(Value)
-		print(Value)
+		print(Value);
 	end;
 });
 General:AddSlider('SliderFlag1', {
@@ -43,9 +43,17 @@ General:AddSlider('SliderFlag1', {
 	Min = 5;
 	Max = 65;
 	Callback = function(Value)
-      		print(Value)
+      		print(Value);
 	end;
 });
+General:AddDropdown('DropdownFlag1', {
+	Text = 'Dropdown 1';
+	Default = 'Value One';
+	Values = {'Value 1', 'Value 2', 'Value 3'};
+	Callback = function(Value)
+		print(Value);
+	end;
+})
 General:AddLabel({
 	Text = 'Label 1';
 })
